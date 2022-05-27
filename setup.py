282e4117b3fd973b3086a68d34914af9157b9a69
gleb <admin@glebmail.xyz>
2022-05-27 20:29:48 +0300

    edit path

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='pcsd',
-    version='1.0',
+    version='1.1',
     description='Pcsd server',
     url='https://git.glebmail.xyz/PythonPrograms/pcsd',
     author='gleb',
