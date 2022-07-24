3f49c5841d7f5ce3ca99a04ab897c4945d18441a
gleb <gleb@glebmail.xyz>
2022-07-24 17:17:50 +0300

    add plugins

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pcsd',
-    version='2.2',
+    version='2.3',
     description='Pcsd server',
     url='https://git.glebmail.xyz/PythonPrograms/pcsd',
     author='gleb',
